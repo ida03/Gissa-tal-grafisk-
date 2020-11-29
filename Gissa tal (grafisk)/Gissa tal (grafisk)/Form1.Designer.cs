a60a217a76be21bc1f858e919782e75930d7ab82
Ida <idsp03001@utb.vaxjo.se>
2020-11-29 20:53:47 +0100

    Skrivit in all kod

---
+++ Gissa tal (grafisk)/Gissa tal (grafisk)/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Gissa_tal__grafisk_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textboxgissning = new System.Windows.Forms.TextBox();
+            this.textboxresultat = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(125, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Gissa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textboxgissning
+            // 
+            this.textboxgissning.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textboxgissning.Location = new System.Drawing.Point(91, 85);
+            this.textboxgissning.Multiline = true;
+            this.textboxgissning.Name = "textboxgissning";
+            this.textboxgissning.Size = new System.Drawing.Size(172, 35);
+            this.textboxgissning.TabIndex = 1;
+            // 
+            // textboxresultat
+            // 
+            this.textboxresultat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textboxresultat.Location = new System.Drawing.Point(12, 167);
+            this.textboxresultat.Multiline = true;
+            this.textboxresultat.Name = "textboxresultat";
+            this.textboxresultat.Size = new System.Drawing.Size(334, 271);
+            this.textboxresultat.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ange ett heltal mellan 1 och 100";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textboxresultat);
+            this.Controls.Add(this.textboxgissning);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Gissa tal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textboxgissning;
+        private System.Windows.Forms.TextBox textboxresultat;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Gissa tal (grafisk)/Gissa tal (grafisk)/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gissa_tal__grafisk_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random randomerare = new Random();
+            int tal = randomerare.Next(1, 100);
+            int räknare = 0;
+            int gissning = 0;
+
+            while (gissning != tal)
+            {
+                gissning = int.Parse(textboxgissning.Text);
+                räknare++;
+
+                if (gissning != tal)
+                {
+                    if (gissning < tal)
+                        textboxresultat.Text = "Gissningen är för låg";
+
+                    if (gissning > tal)
+                        textboxresultat.Text = "Gissningen är för hög";
+                }
+                else
+                    textboxresultat.Text = "Gissningen är rätt!" + " \nAntal gissningar: " + räknare;
+            }
+
+        }
+    }
+}
